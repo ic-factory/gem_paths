5002053023a748776b3b90568d9ec7f2685517a9
Torben Jacobsen <nettet@foxware.dk>
2019-03-27 21:19:14 +0100

    Added 'after-install-all' hook

---
+++ lib/gem_paths/plugin.rb
@@ -0,0 +1,18 @@
+module GemPaths
+  module Plugin
+
+    class << self
+      # Registers the plugin and adds all needed hooks
+      # Will call troubleshoot via the `after-install` hook if the install does not succeed
+      def register
+        return if defined?(@registered) && @registered
+        @registered = true
+
+        Bundler::Plugin.add_hook('after-install-all') do |spec_install|
+          puts "Installation has finished - from gem_paths"
+        end
+      end
+
+    end
+  end
+end
--- plugins.rb
+++ plugins.rb
@@ -1,12 +1,2 @@
-require 'gem_paths'
-
-class MyCommand < Bundler::Plugin::API
-  # Register this class as a handler for the `my_command` command
-  command "my_command"
-
-  # The exec method will be called with the `command` and the `args`.
-  # This is where you should handle all logic and functionality
-  def exec(command, args)
-    puts "You called " + command + " with args: " + args.inspect
-  end
-end
+require 'gem_paths/plugin'
+GemPaths::Plugin.register
