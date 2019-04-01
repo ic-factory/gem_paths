e67f7f34989b702a810913aa9177c22de3cde62d
Torben Jacobsen <nettet@foxware.dk>
2019-04-01 22:56:49 +0200

    A gem_paths.make file in the .bundler directory is now created/updated each time 'bundle install' is called.

--- lib/gem_paths/plugin.rb
+++ lib/gem_paths/plugin.rb
@@ -1,4 +1,5 @@
 require 'gem_paths/list'
+require 'fileutils'
 
 module GemPaths
   module Plugin
@@ -9,11 +10,11 @@ module GemPaths
       def register
         return if defined?(@registered) && @registered
         @registered = true
-
         Bundler::Plugin.add_hook('after-install-all') do |spec_install|
-          puts "Installation has finished. These are the paths to your gems:"
-          puts GemPaths::List.to_make
-          puts ""
+          puts "Updating #{gem_paths_make_file} ..."
+          gem_paths_make_file = (Bundler::SharedHelpers.root + '.bundle' + 'gem_paths.make')
+          FileUtils.mkdir_p gem_paths_make_file.dirname unless File.directory?(gem_paths_make_file.dirname)
+          File.write(gem_paths_make_file, GemPaths::List.to_make)
         end
       end
 
