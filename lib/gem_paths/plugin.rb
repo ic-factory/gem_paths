13d7ff7c455f7a695901c8af1627653b68bc51f2
Torben Jacobsen <nettet@foxware.dk>
2019-04-01 23:03:15 +0200

    Fixed typo

--- lib/gem_paths/plugin.rb
+++ lib/gem_paths/plugin.rb
@@ -11,8 +11,8 @@ module GemPaths
         return if defined?(@registered) && @registered
         @registered = true
         Bundler::Plugin.add_hook('after-install-all') do |spec_install|
-          puts "Updating #{gem_paths_make_file} ..."
           gem_paths_make_file = (Bundler::SharedHelpers.root + '.bundle' + 'gem_paths.make')
+          puts "Updating #{gem_paths_make_file} ..."
           FileUtils.mkdir_p gem_paths_make_file.dirname unless File.directory?(gem_paths_make_file.dirname)
           File.write(gem_paths_make_file, GemPaths::List.to_make)
         end
