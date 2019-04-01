bdfd9c922b1c62b8062ea505913fe9c5d721c96f
Torben Jacobsen <nettet@foxware.dk>
2019-04-01 23:07:04 +0200

    gem_paths.make file is now written with one file per line

--- lib/gem_paths/plugin.rb
+++ lib/gem_paths/plugin.rb
@@ -14,7 +14,7 @@ module GemPaths
           gem_paths_make_file = (Bundler::SharedHelpers.root + '.bundle' + 'gem_paths.make')
           puts "Updating #{gem_paths_make_file} ..."
           FileUtils.mkdir_p gem_paths_make_file.dirname unless File.directory?(gem_paths_make_file.dirname)
-          File.write(gem_paths_make_file, GemPaths::List.to_make)
+          File.write(gem_paths_make_file, GemPaths::List.to_make.join("\n"))
         end
       end
 
