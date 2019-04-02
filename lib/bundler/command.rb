e31b3c5c5975257a9aa22497d579d6a1f251ca53
Torben Jacobsen <nettet@foxware.dk>
2019-04-02 22:47:25 +0200

    Added functionality to 'gem_paths' bundler command

--- lib/bundler/command.rb
+++ lib/bundler/command.rb
@@ -1,9 +1,15 @@
+require 'gem_paths/list'
+require 'fileutils'
+
 module Bundler
   class Command
     Plugin::API.command('gem_paths', self)
 
     def exec(command_name, args)
-      puts "You called " + command_name
+      gem_paths_make_file = (Bundler::SharedHelpers.root + '.bundle' + 'gem_paths.make')
+      puts "Updating #{gem_paths_make_file} ..."
+      FileUtils.mkdir_p gem_paths_make_file.dirname unless File.directory?(gem_paths_make_file.dirname)
+      File.write(gem_paths_make_file, GemPaths::List.to_make.join("\n") + "\n")
     end
   end
 end
